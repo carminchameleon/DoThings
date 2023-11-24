d2ba8fff07c9c158bad586617019911d33ea34a1
carminchameleon <hwanggee1211@gmail.com>
2023-11-24 23:15:04 +1100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Eunji Hwang on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
